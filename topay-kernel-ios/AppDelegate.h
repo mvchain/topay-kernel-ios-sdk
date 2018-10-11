2b8385d2ed8afc015572e0694d51fc5a238eae2d
fengKun <2426905749@qq.com>
2018-10-11 19:55:42 +0800

    1.0

---
+++ topay-kernel-ios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  topay-kernel-ios
+//
+//  Created by 蒲公英&守候 on 2018/10/11.
+//  Copyright © 2018年 蒲公英&守候. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ topay-kernel-ios/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  topay-kernel-ios
+//
+//  Created by 蒲公英&守候 on 2018/10/11.
+//  Copyright © 2018年 蒲公英&守候. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
