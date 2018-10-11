debd1fbf2cb56b9e779f4eb24cd9432a8db23157
fengKun <2426905749@qq.com>
2018-10-11 19:59:12 +0800

    test

---
+++ topay-kernel-ios/MVCTest.h
@@ -0,0 +1,13 @@
+//
+//  MVCTest.h
+//  topay-kernel-ios
+//
+//  Created by 蒲公英&守候 on 2018/10/11.
+//  Copyright © 2018年 蒲公英&守候. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MVCTest : NSObject
+
+@end
---
+++ topay-kernel-ios/MVCTest.m
@@ -0,0 +1,23 @@
+//
+//  MVCTest.m
+//  topay-kernel-ios
+//
+//  Created by 蒲公英&守候 on 2018/10/11.
+//  Copyright © 2018年 蒲公英&守候. All rights reserved.
+//
+
+#import "MVCTest.h"
+
+@implementation MVCTest
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        NSLog(@"ddasd");
+    }
+    return self;
+}
+
+
+@end
